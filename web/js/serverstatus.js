07f501fde7ff5b8646c109cc0287d074102c7d88
DX.Kevin <33936992+DX-Kevin@users.noreply.github.com>
2023-08-19 02:36:56 +0000

    Update serverstatus.js

--- web/js/serverstatus.js
+++ web/js/serverstatus.js
@@ -12,29 +12,10 @@ function timeSince(date) {
 	if (interval > 1)
 		return interval + " 分钟前.";
 	else
-		return "几秒前.";
+		return " 1 秒前.";
 }
 
-function getTime(){
-        //获取日期类对象
-        var date = new Date;
-        //利用get方法获取当前年、月、日、时、分、秒等信息，并拼接在一起。
-        //注意：这里的date.getMonth()得到的月份数是从0开始的，所以要+1。
-        var time = date.getFullYear()+ "年" + 
-                    (date.getMonth() + 1) + "月" +
-                    date.getDay() + "日" + 
-                    date.getHours() + "时" + 
-                    date.getMinutes() + "分" + 
-                    date.getSeconds() + "秒";
-        //根据ID获取对应的span标签类对象
-        var showTimeSpan = document.getElementById("showTime");
-        //innerHTML
-        //语法：HTMLElementObject.innerHTML = text
-        //作用：改变同一行的一对HTML标签中的值
-        showTimeSpan.innerHTML = time;
-    }
-    //setInterval，定时调用某个方法，第一个参数："方法名()"，第二个参数为毫秒数。
-    window.setInterval("getTime()", 1000);
+
 
 function bytesToSize(bytes, precision, si)
 {
@@ -344,16 +325,48 @@ function uptime() {
 	});
 }
 
+function getTime() {
+	//获取日期类对象
+	var date = new Date;
+	//利用get方法获取当前年、月、日、时、分、秒等信息，并拼接在一起。
+	//注意：这里的date.getMonth()得到的月份数是从0开始的，所以要+1。
+	var time = date.getFullYear() + "年" +
+		(date.getMonth() + 1) + "月" +
+		date.getDay() + "日" +
+		date.getHours() + "时" +
+		date.getMinutes() + "分" +
+		date.getSeconds() + "秒";
+	//根据ID获取对应的span标签类对象
+	var showTimeSpan = document.getElementById("showTime");
+	//innerHTML
+	//语法：HTMLElementObject.innerHTML = text
+	//作用：改变同一行的一对HTML标签中的值
+	showTimeSpan.innerHTML = time;
+}
+
+//setInterval，定时调用某个方法，第一个参数："方法名()"，第二个参数为毫秒数。
+window.setInterval("getTime()", 1000);
+
 function updateTime() {
+	//获取日期类对象
+	var date = new Date;
+	//利用get方法获取当前年、月、日、时、分、秒等信息，并拼接在一起。
+	//注意：这里的date.getMonth()得到的月份数是从0开始的，所以要+1。
+	var time = date.getFullYear() + "年" +
+		(date.getMonth() + 1) + "月" +
+		date.getDay() + "日" +
+		date.getHours() + "时" +
+		date.getMinutes() + "分" +
+		date.getSeconds() + "秒";
 	if (!error)
-		$("#updated").html("最后更新: " + timeSince(d));
+		//$("#updated").html("现在时间: " + time + " | 最后更新时间: " + timeSince(d));
+		$("#updated").html("最后更新时间: " + timeSince(d));
 }
 
 uptime();
 updateTime();
-setInterval(uptime, 2000);
-setInterval(updateTime, 2000);
-
+setInterval(uptime, 1000);
+setInterval(updateTime, 1000);
 
 // styleswitcher.js
 function setActiveStyleSheet(title, cookie=false) {
